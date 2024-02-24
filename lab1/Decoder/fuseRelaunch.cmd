d375cc2c064a6e3ae44a28b50107d3655b0d5e83
OrestHu <147592134+OrestHu@users.noreply.github.com>
2024-02-24 15:59:20 +0200

    lab1

---
+++ lab1/Decoder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Decoder/Decoder/My2To4Decoder_isim_beh.exe" -prj "C:/Decoder/Decoder/My2To4Decoder_beh.prj" "work.My2To4Decoder" "work.glbl" 
